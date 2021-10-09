df7b155d8591a99999b0a928ad89710792e6132a
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-09 21:57:03 +0300

    Setup properly the $smarty object in Smarty2\Tests\TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,13 +5,31 @@ namespace Smarty2\Tests;
 use Smarty2\Engine;
 use PHPUnit\Framework\TestCase as PHPUnit_TestCase;
 
+use function is_dir;
+use function mkdir;
+use function sys_get_temp_dir;
+
 class TestCase extends PHPUnit_TestCase
 {
         protected Engine $smarty;
 
         function setUp() : void
         {
-                $this->smarty = new Engine;
+                $smarty = new Engine;
+
+                $smarty->template_dir = __DIR__ . '/templates';
+                
+                $smarty->compile_dir = sys_get_temp_dir() . '/smarty.compiled';
+                if (!is_dir($smarty->compile_dir))
+                {
+                        mkdir($smarty->compile_dir,
+                                $smarty->_dir_perms,
+                                true);
+                }
+
+                $smarty->compile_check = true;
+
+                $this->smarty = $smarty;
         }
 
         function tearDown() : void
