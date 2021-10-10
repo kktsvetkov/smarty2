804ea891ca40a63c34aab33db9a3c30cb067d45c
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-10 17:58:50 +0300

    More assign tests added in Smarty2\Tests\VarsTest

--- tests/VarsTest.php
+++ tests/VarsTest.php
@@ -19,4 +19,98 @@ class VarsTest extends TestCase
 			$_SERVER['SCRIPT_NAME']
 		);
 	}
+
+	/**
+	* @covers Smarty2\Engine::clear_assign()
+        * @covers Smarty2\Engine::assign()
+        * @covers Smarty2\Engine::get_template_vars()
+	*/
+	function testAssignScalars()
+	{
+		// start fresh
+		//
+		$this->smarty->clear_assign('foo');
+		$this->assertNull($this->smarty->get_template_vars('foo'));
+
+		// first assign
+		//
+		$this->smarty->assign('foo', 'bar');
+		$this->assertEquals($this->smarty->get_template_vars('foo'), 'bar');
+
+		// overwrite it and test again
+		//
+		$this->smarty->assign('foo', 'bar!');
+		$this->assertEquals($this->smarty->get_template_vars('foo'), 'bar!');
+
+		// clean up and test again
+		//
+		$this->smarty->clear_assign('foo');
+		$this->assertNull($this->smarty->get_template_vars('foo'));
+	}
+
+	/**
+	* @covers Smarty2\Engine::clear_assign()
+        * @covers Smarty2\Engine::assign()
+        * @covers Smarty2\Engine::get_template_vars()
+	*/
+	function testAssignArrays()
+	{
+		// start fresh
+		//
+		$this->smarty->clear_assign('foo');
+		$this->assertNull($this->smarty->get_template_vars('foo'));
+
+		// first array assign
+		//
+		$this->smarty->assign(array('foo' => 'bar'));
+		$this->assertEquals($this->smarty->get_template_vars('foo'), 'bar');
+
+		// overwrite and test again
+		//
+		$this->smarty->assign(array('foo' => 'bar!'));
+		$this->assertEquals($this->smarty->get_template_vars('foo'), 'bar!');
+	}
+
+	/**
+        * @covers Smarty2\Engine::assign()
+        * @covers Smarty2\Engine::get_template_vars()
+	*/
+	function testAssignTemplateVars()
+	{
+		$this->smarty->assign('foo', 'bar');
+		$this->assertEquals(
+			$this->smarty->fetch('AssignTemplateVars.tpl'),
+			'bar'
+			);
+
+		$this->smarty->assign('foo', '');
+		$this->assertEquals(
+			$this->smarty->fetch('AssignTemplateVars.tpl'),
+			''
+			);
+
+		$this->smarty->assign('foo', array());
+		$this->assertEquals(
+			$this->smarty->fetch('AssignTemplateVars.tpl'),
+			'Array'
+			);
+
+		// $this->smarty->assign('foo', new \stdClass);
+		// $this->assertEquals(
+		// 	$this->smarty->fetch('AssignTemplateVars.tpl'),
+		// 	'stdClass'
+		// 	);
+
+		$this->smarty->assign('foo', new class()
+		{
+			function __toString()
+			{
+				return 'Proba!';
+			}
+		});
+		$this->assertEquals(
+			$this->smarty->fetch('AssignTemplateVars.tpl'),
+			'Proba!'
+			);
+	}
 }
