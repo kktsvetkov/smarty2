0527cf1e231b726b8643b08499e84eca0b450e76
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-10 13:55:42 +0300

    Do not case objects and arrays as strings for smarty_modifier_escape()

--- plugins/modifier.escape.php
+++ plugins/modifier.escape.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Smarty plugin
- * @package Smarty
- * @subpackage plugins
- */
-
 
 /**
  * Smarty escape modifier plugin
@@ -12,83 +6,121 @@
  * Type:     modifier<br>
  * Name:     escape<br>
  * Purpose:  Escape the string according to escapement type
- * @link http://smarty.php.net/manual/en/language.modifier.escape.php
- *          escape (Smarty online manual)
- * @author   Monte Ohrt <monte at ohrt dot com>
- * @param string
- * @param html|htmlall|url|quotes|hex|hexentity|javascript
+ * @param string $subject
+ * @param string $esc_type html|htmlall|url|quotes|hex|hexentity|javascript
+ * @param string $char_set
  * @return string
  */
-function smarty_modifier_escape($string, $esc_type = 'html', $char_set = 'ISO-8859-1')
+function smarty_modifier_escape($subject, $esc_type = 'html', $char_set = 'ISO-8859-1')
 {
-        $string = (string) $string;
-    switch ($esc_type) {
-        case 'html':
-            return htmlspecialchars($string, ENT_QUOTES, $char_set);
+        // escape only objects that can be made into strings
+        //
+        if (is_object($subject))
+        {
+                if (is_callable([$subject, '__toString']))
+                {
+                        $subject = $subject->__toString();
+                } else
+                {
+                        return $subject;
+                }
+        }
 
-        case 'htmlall':
-            return htmlentities($string, ENT_QUOTES, $char_set);
+        // escape each array member individually
+        //
+        if (is_array($subject))
+        {
+                foreach ($subject as &$element)
+                {
+                        $element = smarty_modifier_escape(
+                                $element,
+                                $esc_type,
+                                $char_set);
+                }
 
-        case 'url':
-            return rawurlencode($string);
+                return $subject;
+        }
 
-        case 'urlpathinfo':
-            return str_replace('%2F','/',rawurlencode($string));
+        $subject = (string) $subject;
+        switch ($esc_type)
+        {
+                case 'html':
+                        return htmlspecialchars($subject, ENT_QUOTES, $char_set);
 
-        case 'quotes':
-            // escape unescaped single quotes
-            return preg_replace("%(?<!\\\\)'%", "\\'", $string);
+                case 'htmlall':
+                        return htmlentities($subject, ENT_QUOTES, $char_set);
 
-        case 'hex':
-            // escape every character into hex
-            $return = '';
-            for ($x=0; $x < strlen($string); $x++) {
-                $return .= '%' . bin2hex($string[$x]);
-            }
-            return $return;
+                case 'url':
+                        return rawurlencode($subject);
 
-        case 'hexentity':
-            $return = '';
-            for ($x=0; $x < strlen($string); $x++) {
-                $return .= '&#x' . bin2hex($string[$x]) . ';';
-            }
-            return $return;
+                case 'urlpathinfo':
+                        return str_replace('%2F','/',rawurlencode($subject));
 
-        case 'decentity':
-            $return = '';
-            for ($x=0; $x < strlen($string); $x++) {
-                $return .= '&#' . ord($string[$x]) . ';';
-            }
-            return $return;
+                case 'quotes':
+                        // escape unescaped single quotes
+                        return preg_replace("%(?<!\\\\)'%", "\\'", $subject);
 
-        case 'javascript':
-            // escape quotes and backslashes, newlines, etc.
-            return strtr($string, array('\\'=>'\\\\',"'"=>"\\'",'"'=>'\\"',"\r"=>'\\r',"\n"=>'\\n','</'=>'<\/'));
+                case 'hex':
+                        // escape every character into hex
+                        $return = '';
+                        for ($x=0; $x < strlen($subject); $x++)
+                        {
+                                $return .= '%' . bin2hex($subject[$x]);
+                        }
+                        return $return;
 
-        case 'mail':
-            // safe way to display e-mail address on a web page
-            return str_replace(array('@', '.'),array(' [AT] ', ' [DOT] '), $string);
+                case 'hexentity':
+                        $return = '';
+                        for ($x=0; $x < strlen($subject); $x++)
+                        {
+                                $return .= '&#x' . bin2hex($subject[$x]) . ';';
+                        }
+                        return $return;
 
-        case 'nonstd':
-           // escape non-standard chars, such as ms document quotes
-           $_res = '';
-           for($_i = 0, $_len = strlen($string); $_i < $_len; $_i++) {
-               $_ord = ord(substr($string, $_i, 1));
-               // non-standard char, escape it
-               if($_ord >= 126){
-                   $_res .= '&#' . $_ord . ';';
-               }
-               else {
-                   $_res .= substr($string, $_i, 1);
-               }
-           }
-           return $_res;
+                case 'decentity':
+                        $return = '';
+                        for ($x=0; $x < strlen($subject); $x++)
+                        {
+                                $return .= '&#' . ord($subject[$x]) . ';';
+                        }
+                        return $return;
 
-        default:
-            return $string;
-    }
+                case 'javascript':
+                        // escape quotes and backslashes, newlines, etc.
+                        return strtr($subject, array(
+                                '\\'=>'\\\\',
+                                "'"=>"\\'",
+                                '"'=>'\\"',
+                                "\r"=>'\\r',
+                                "\n"=>'\\n',
+                                '</'=>'<\/'
+                        ));
+
+                case 'mail':
+                        // safe way to display e-mail address on a web page
+                        return str_replace(
+                                array('@', '.'),
+                                array(' [AT] ', ' [DOT] '),
+                                $subject);
+
+                case 'nonstd':
+                        // escape non-standard chars, such as ms document quotes
+                        $_res = '';
+                        for($_i = 0, $_len = strlen($subject); $_i < $_len; $_i++)
+                        {
+                                $_ord = ord(substr($subject, $_i, 1));
+                                // non-standard char, escape it
+                                if($_ord >= 126)
+                                {
+                                        $_res .= '&#' . $_ord . ';';
+                                } else
+                                {
+                                $_res .= substr($subject, $_i, 1);
+                                }
+                        }
+
+                        return $_res;
+        }
+
+        return $subject;
 }
-
-/* vim: set expandtab: */
-
-?>
