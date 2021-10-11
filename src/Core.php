38a16e58fddb4817f32bd6435174b6a23bf9be81
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-11 05:10:53 +0300

    Dropping Smarty2\Core::assemple_plugin_filepath(), use LegacyProvider instead

--- src/Core.php
+++ src/Core.php
@@ -12,45 +12,6 @@ namespace Smarty2;
 class Core
 {
 	/**
-	 * assemble filepath of requested plugin
-	 *
-	 * @param string $type
-	 * @param string $name
-	 * @return string|false
-	 */
-	static function assemble_plugin_filepath($params, &$smarty)
-	{
-	    $_plugin_filename = $params['type'] . '.' . $params['name'] . '.php';
-	    if (isset($smarty->_filepaths_cache[$_plugin_filename])) {
-		return $smarty->_filepaths_cache[$_plugin_filename];
-	    }
-	    $_return = false;
-
-	    foreach ((array)$smarty->plugins_dir as $_plugin_dir) {
-
-		$_plugin_filepath = $_plugin_dir . DIRECTORY_SEPARATOR . $_plugin_filename;
-
-		// see if path is relative
-		if (!preg_match("/^([\/\\\\]|[a-zA-Z]:[\/\\\\])/", $_plugin_dir)) {
-		    $_relative_paths[] = $_plugin_dir;
-		    // relative path, see if it is in the SMARTY_DIR
-		    if (@is_readable(SMARTY_DIR . $_plugin_filepath)) {
-			$_return = SMARTY_DIR . $_plugin_filepath;
-			break;
-		    }
-		}
-		// try relative to cwd (or absolute)
-		if (@is_readable($_plugin_filepath)) {
-		    $_return = $_plugin_filepath;
-		    break;
-		}
-	    }
-
-	    $smarty->_filepaths_cache[$_plugin_filename] = $_return;
-	    return $_return;
-	}
-
-	/**
 	 * Load requested plugins
 	 *
 	 * @param array $plugins
