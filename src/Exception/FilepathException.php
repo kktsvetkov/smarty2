f79d134728e24e0d95c907b143fe1f32b80309e5
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-11 04:24:11 +0300

    Updating Smarty2\Exception

--- src/Exception/FilepathException.php
+++ src/Exception/FilepathException.php
@@ -6,16 +6,16 @@ use RuntimeException;
 
 class FilepathException extends RuntimeException
 {
-        protected string $filepath;
+	protected string $filepath;
 
-        function __construct(string $message, string $filepath)
-        {
-                $this->filepath = $filepath;
-                parent::__construct($message);
-        }
+	function __construct(string $message, string $filepath)
+	{
+		$this->filepath = $filepath;
+		parent::__construct($message);
+	}
 
-        function getFilepath() : string
-        {
-                return $this->filepath;
-        }
+	function getFilepath() : string
+	{
+		return $this->filepath;
+	}
 }
--- src/Exception/SyntaxException.php
+++ src/Exception/SyntaxException.php
@@ -8,41 +8,41 @@ use function sprintf;
 
 class SyntaxException extends UnexpectedValueException
 {
-        protected string $templateMessage;
-        protected string $templateName;
-        protected int $templateLine;
+	protected string $templateMessage;
+	protected string $templateName;
+	protected int $templateLine;
 
-        function __construct(string $message, string $templateName, int $templateLine)
-        {
-                $this->templateMessage = $message;
-                $this->templateName = $templateName;
-                $this->templateLine = $templateLine;
+	function __construct(string $message, string $templateName, int $templateLine)
+	{
+		$this->templateMessage = $message;
+		$this->templateName = $templateName;
+		$this->templateLine = $templateLine;
 
-                parent::__construct( $this->getTemplateError() );
-        }
+		parent::__construct( $this->getTemplateError() );
+	}
 
-        function getTemplateError() : string
-        {
-                return sprintf(
-                        '%s [in %s line %d]',
-                        $this->templateMessage,
-                        $this->templateName,
-                        $this->templateLine
-                );
-        }
+	function getTemplateError() : string
+	{
+		return sprintf(
+			'%s [in %s line %d]',
+			$this->templateMessage,
+			$this->templateName,
+			$this->templateLine
+		);
+	}
 
-        function getTemplateMessage() : string
-        {
-                return $this->templateMessage;
-        }
+	function getTemplateMessage() : string
+	{
+		return $this->templateMessage;
+	}
 
-        function getTemplateName() : string
-        {
-                return $this->templateName;
-        }
+	function getTemplateName() : string
+	{
+		return $this->templateName;
+	}
 
-        function getTemplteLine() : int
-        {
-                return $this->templateLine;
-        }
+	function getTemplteLine() : int
+	{
+		return $this->templateLine;
+	}
 }
