995948866ae84cbf65665dd3c53f1c36ad541016
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-11 11:09:30 +0300

    Dropping Smarty2\Engine::$_dir_perms and Smarty2\Engine::$_file_perms

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,8 +5,6 @@ namespace Smarty2\Tests;
 use Smarty2\Engine;
 use PHPUnit\Framework\TestCase as PHPUnit_TestCase;
 
-use function is_dir;
-use function mkdir;
 use function sys_get_temp_dir;
 
 class TestCase extends PHPUnit_TestCase
@@ -20,12 +18,6 @@ class TestCase extends PHPUnit_TestCase
 		$smarty->template_dir = __DIR__ . '/templates';
 
 		$smarty->compile_dir = sys_get_temp_dir() . '/smarty.compiled';
-		if (!is_dir($smarty->compile_dir))
-		{
-			mkdir($smarty->compile_dir,
-				$smarty->_dir_perms,
-				true);
-		}
 
 		$smarty->compile_check = true;
 
