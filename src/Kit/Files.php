20344e736cecb50abfc7407f244818b075581df7
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-13 14:20:24 +0300

    Adding Smarty2\Kit\Files::isAbsolute() and Smarty2\Kit\Files::isRelative()

--- src/Kit/Files.php
+++ src/Kit/Files.php
@@ -5,6 +5,7 @@ namespace Smarty2\Kit;
 use function filemtime;
 use function is_file;
 use function time;
+use function preg_match;
 use function unlink;
 
 class Files
@@ -30,5 +31,23 @@ class Files
 		}
 
 		return unlink($resource);
+	}
+
+	/**
+	* Whether $filepath is an absolute file address
+	* @return boolean
+	*/
+	static function isAbsolute(string $filepath) : bool
+	{
+		return preg_match('/^([\/\\\\]|[a-zA-Z]:[\/\\\\])/', $filepath);
+	}
+
+	/**
+	* Whether $filepath is a relative file address 
+	* @return boolean
+	*/
+	static function isRelative(string $filepath) : bool
+	{
+		return !self::isAbsolute($filepath);
 	}
 }
