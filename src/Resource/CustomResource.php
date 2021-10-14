c9445ab92bfe01f0fb2535935dc1bce1809a3df4
Kaloyan Tsvetkov <kaloyan@gmail.com>
2021-10-14 11:39:47 +0300

    Change Smarty2\Resource\CustomResource to take $smarty as argument

--- src/Resource/CustomResource.php
+++ src/Resource/CustomResource.php
@@ -2,17 +2,53 @@
 
 namespace Smarty2\Resource;
 
+use Smarty2\Engine as Smarty;
 use Smarty2\Exception;
 use Smarty2\Resource\ResourceInterface;
-use Smarty2\Resource\CallbackResourceTrait;
 
 class CustomResource implements ResourceInterface
 {
-	use CallbackResourceTrait;
+	protected $sourceCallback;
+	protected $timestampCallback;
 
-	function __construct(callable $sourceCallback, callable $timestampCallback)
+	function __construct(
+		callable $sourceCallback,
+		callable $timestampCallback,
+		Smarty $smarty)
 	{
-		$this->setSourceCallback($sourceCallback);
-		$this->setTimestampCallback($timestampCallback);
+		$this->sourceCallback = $sourceCallback;
+		$this->timestampCallback = $timestampCallback;
+		$this->smarty = $smarty;
+	}
+
+	function templateExists(string $name) : bool
+	{
+		try {
+			return $this->getTemplateSource($name)
+				&& $this->getTemplateTimestamp($name);
+		}
+		catch (Exception\TemplateNotFoundException $e)
+		{
+		}
+
+		return false;
+	}
+
+	function getTemplateSource(string $name) : string
+	{
+		$sourceCallback = $this->sourceCallback;
+		$contents = '';
+
+		$sourceCallback( $name, &$contents, $this->smarty );
+		return $contents;
+	}
+
+	function getTemplateTimestamp(string $name) : int
+	{
+		$timestampCallback = $this->timestampCallback;
+		$timestamp = 0;
+
+		$timestampCallback( $name, &$timestamp, $this->smarty );
+		return $timestamp;
 	}
 }
